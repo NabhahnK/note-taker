704b1fb93af2d24fa3a3738c321cf335bb5e2a02
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-07-17 11:02:48 -0700

    added route for notes.html

--- server.js
+++ server.js
@@ -12,4 +12,9 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET Route for notes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
